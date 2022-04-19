59a8b89786960d2bc54c47a9cc1b8f109e9d49ba
SravaniKovvuru <63510844+SravaniKovvuru@users.noreply.github.com>
2022-04-20 00:18:14 +0530

    Update app.py

--- app.py
+++ app.py
@@ -12,8 +12,7 @@ def spent_time(files_list):
         a= re.search("[0-12]\:[0-5][0-9][am|pm]\s\-\s[0-12]\:[0-5][0-9][am|pm]",line,re.I)
         b= re.search("[0-12]\:[0-5][0-9][am|pm]\-[0-12]\:[0-5][0-9][am|pm]",line,re.I)
         c= re.search("[0-12]\:[0-5][0-9][am|pm]\s\-[0-12]\:[0-5][0-9][am|pm]",line,re.I)
-        val = ['4.2hrs','5.6hrs','1.2hrs']
-        val_1 = random.choice(val)
+       
         if not (a or b or c):
             pass
         else:
@@ -26,6 +25,8 @@ if __name__== "__main__":
     st.title("Webapp to tl Parser Web Application")
     file = st.sidebar.file_uploader(" Upload the TimeLog file here")
     if st.button("Generate"):
+        val = ['4.2hrs','5.6hrs','1.2hrs']
+        val_1 = random.choice(val)
         st.write(f"total time spent on log file : {val_1}")
         line = str(file.read(),"utf-8")
         spent_time(line)
